fe20b55e982eab3f2fa2bd71c14a1078ff53a81a
adun0008 <adun0008@student.monash.edu>
2023-08-17 13:34:01 +1000

    Add.php fixes.

--- templates/Contents/add.php
+++ templates/Contents/add.php
@@ -4,70 +4,267 @@
  * @var \App\Model\Entity\Content $content
  * @var \Cake\Collection\CollectionInterface|string[] $tickets
  */
+
+$this->disableAutoLayout();
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 
-<!-- include libraries(jQuery, bootstrap) -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-<!-- include summernote css/js -->
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<head>
+    <?= $this->Html->meta('icon', 'favicon.ico', ['type' => 'icon']) ?>
 
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Contents'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>GamBlock® - Customer Management: Contents add</title>
+    <!-- Bootstrap CSS -->
+    <!-- In-built CSS -->
+    <?= $this->Html->css(['style', 'bootstrap.min',]) ?>
+    <?= $this->Html->css(['fontawesome-all'], ['block' => true]) ?>
+
+
+
+
+
+</head>
+
+
+<!-- ============================================================== -->
+<!-- main wrapper -->
+<!-- ============================================================== -->
+<div class="dashboard-main-wrapper">
+    <!-- ============================================================== -->
+    <!-- navbar -->
+    <!-- ============================================================== -->
+    <div class="dashboard-header">
+        <nav class="navbar navbar-expand-lg bg-white fixed-top">
+            <a class="navbar-brand" href="/">
+                <?= $this->Html->image('cake-logo.png', ['alt' => 'GamBlock Logo', 'class' => 'navbar-brand', 'style' => 'width: 225px; height: auto;']); ?> -Staff Portal
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto navbar-right-top">
+                    <a href="#"><i class="fas fa-power-off mr-2"></i> <?php echo $this->Html->link(__('Logout'), ['controller' => 'Auth', 'action' => 'logout']); ?></a>
+
+
+                    <!--                    <li class="nav-item dropdown nav-user">-->
+                    <!--                        <a class="nav-link nav-user-file" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>-->
+                    <!--                        <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">-->
+                    <!--                            <div class="nav-user-info">-->
+                    <!--                                <h5 class="mb-0 text-white nav-user-name">-->
+                    <!--                                    Example User</h5>-->
+                    <!--                            </div>-->
+                    <!--                            <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i> --><?php //echo $this->Html->link(__('Logout'), ['controller' => 'Auth', 'action' => 'logout']); ?><!--</a>-->
+                    <!--                        </div>-->
+                    <!--                    </li>-->
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <!-- ============================================================== -->
+    <!-- end navbar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- left sidebar -->
+    <!-- ============================================================== -->
+    <div class="nav-left-sidebar sidebar-dark">
+        <div class="menu-list">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="d-xl-none d-lg-none" href="#">Customer View</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav flex-column">
+                        <li class="nav-divider">
+                            Menu
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Customer Management <span class="badge badge-success">6</span></a>
+                            <div id="submenu-1" class="collapse submenu" style="">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/">Assigned to me</a>
+                                        <!--                                        Change my link to assigned to me page when done.-->
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/customers">View All</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/customers/add">Add a Customer Profile</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <!--                        <li class="nav-item">-->
+                        <!--                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa-solid fa-user-tag"></i>Tag Management</a>-->
+                        <!--                            <div id="submenu-2" class="collapse submenu" style="">-->
+                        <!--                                <ul class="nav flex-column">-->
+                        <!--                                    <li class="nav-item">-->
+                        <!--                                        <a class="nav-link" href="/Tags/index">View All Tags<span class="badge badge-secondary">New</span></a>-->
+                        <!--                                    </li>-->
+                        <!--                                    <li class="nav-item">-->
+                        <!--                                        <a class="nav-link" href="/Tags/add">Add some Tags<span class="badge badge-secondary">New</span></a>-->
+                        <!--                                    </li>-->
+                        <!--                                </ul>-->
+                        <!--                            </div>-->
+                        <!--                        </li>-->
+                        <li class="nav-divider">
+                            Admin Features
+                            <!--                            Change to me admin only visable.-->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-6" aria-controls="submenu-6"><i class="fa-solid fa-user-tie"></i>Staff Management</a>
+                            <div id="submenu-6" class="collapse submenu" style="">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/Users/">View All Staff Accounts </a>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+
+                    </ul>
+                </div>
+            </nav>
         </div>
-        <?= $this->Html->link(__('Go back'), ['controller' => 'Customers', 'action' => 'view', $custId], ['class' => 'btn btn-rounded btn-primary']) ?>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="contents form content">
-            <?= $this->Form->create($content, ['type'=> 'file']) ?>
-            <fieldset>
-                <legend>
-                    <?= __('Add content for customer: ' . $fullName) ?>
-                    <br>
-                    <?= __('Ticket Id: ' . $ticketId) ?>
-                </legend>
+    </div>
+    <!-- ============================================================== -->
+    <!-- end left sidebar -->
+
+
+    <!-- ============================================================== -->
+
+    <div class="dashboard-wrapper">
+        <div class="container-fluid dashboard-content">
+            <!-- ============================================================== -->
+            <!-- Flash rendering -->
+            <!-- ============================================================== -->
+            <?php
+            // Check if the flash message exists and has content
+            $flashMessage = $this->Flash->render();
+            if (!empty($flashMessage)) {
+                ?>
+                <!-- Flash message, ONLY shows up if ticket is successfully opened/closed -->
+                <div class="alert alert-success" role="alert">
+                    <?= $flashMessage; ?>
+                </div>
                 <?php
-//                    echo $this->Form->control('content');
-//                    echo $this->Form->control('createtime');
-//                    echo $this->Form->control('ticket_id', ['options' => $tickets, 'default' => $ticketId]);
-                //Default value, not very safe
-                //echo $this->Form->text('ticket_id', ['value' => $ticketId, 'readonly' => true, 'class' => 'form-control']);
-                    echo $this->Form->control('content_type', [
+            }
+            ?>
+            <!-- ============================================================== -->
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="section-block" id="basicform">
+                    </div>
+                    <div class="card">
+                        <?= $this->Form->create($content, ['type'=> 'file']) ?>
+                        <fieldset>
+                            <h5 class="card-header">
+                                <legend>
+                                    <?= __('Add content for customer: ' . $fullName) ?>
+                                    <br>
+                                </legend>
+                            </h5>
+                            <?php
+                            //                    echo $this->Form->control('content');
+                            //                    echo $this->Form->control('createtime');
+                            //                    echo $this->Form->control('ticket_id', ['options' => $tickets, 'default' => $ticketId]);
+                            //Default value, not very safe
+                            //echo $this->Form->text('ticket_id', ['value' => $ticketId, 'readonly' => true, 'class' => 'form-control']);
+                            echo $this->Form->control('content_type', [
                                 'type' => 'select',
                                 'options' => $content_types,
                                 //onchange actually exists to call JS
                                 'onchange' => 'switchInput()',
                             ]);
-//                    echo $this->Form->control('file', ['type' => 'file']);
-                    echo '<div id="inputContainer" >';
-                    // The dynamic input field will be added here
-                    echo '</div>';
-                    echo '<!-- Image preview area -->';
-                    echo '<div id="imagePreview" style="display: none;">';
-                    echo '<img id="previewImage" src="#" alt="Image Preview" />';
-                    echo '</div>';
-                ?>
+                            //                    echo $this->Form->control('file', ['type' => 'file']);
+                            echo '<div id="inputContainer" >';
+                            // The dynamic input field will be added here
+                            echo '</div>';
+                            echo '<!-- Image preview area -->';
+                            echo '<div id="imagePreview" style="display: none;">';
+                            echo '<img id="previewImage" src="#" alt="Image Preview" />';
+                            echo '</div>';
+                            ?>
 
-                <!-- Display validation error for the 'image' field -->
-                <?= $this->Form->error('image'); ?>
-                <!-- Display validation error for the 'image' field -->
-                <?= $this->Form->error('file'); ?>
+                            <!-- Display validation error for the 'image' field -->
+                            <?= $this->Form->error('image'); ?>
+                            <!-- Display validation error for the 'image' field -->
+                            <?= $this->Form->error('file'); ?>
 
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+                        </fieldset>
+                        <div class="form-group d-flex justify-content-between align-items-center">
+                            <?= $this->Html->link(__('Return to Customer'), ['controller' => 'Customers', 'action' => 'view', $custId], ['class' => 'btn btn-rounded btn-secondary']) ?>
+                            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
+                        </div>
+                        <?= $this->Form->end() ?>
+                    </div>
+                                        <!-- Uncomment and style the following controls as needed -->
+                                        <!--
+    <div class="form-group">
+        <?= $this->Form->label('createtime', 'Create Time', ['class' => 'col-form-label']) ?>
+        <?= $this->Form->input('createtime', ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <?= $this->Form->label('closetime', 'Close Time', ['class' => 'col-form-label']) ?>
+        <?= $this->Form->input('closetime', ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <?= $this->Form->label('closed', 'Closed', ['class' => 'col-form-label']) ?>
+        <?= $this->Form->input('closed', ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <?= $this->Form->label('cust_id', 'Customer', ['class' => 'col-form-label']) ?>
+        <?= $this->Form->input('cust_id', ['class' => 'form-control', 'options' => $customers]) ?>
+    </div>
+    -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
+    <!--Footer-->
+    <!-- ============================================================== -->
+    <!-- footer -->
+    <!-- ============================================================== -->
+    <div class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                    Copyright ©  GamBlock®. All rights reserved. This site is for access by GamBlock® Staff Only. Template by <a href="https://colorlib.com/wp/">Colorlib</a>.
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                    <!--                                        <div class="text-md-right footer-links d-none d-sm-block">-->
+                    <!--                                            <a href="javascript: void(0);">Documentation</a>-->
+                    <!--                                            <a href="javascript: void(0);">Contact Points</a>-->
+                    <!--                                        </div>-->
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- ============================================================== -->
+    <!-- end footer -->
+    <!-- ============================================================== -->
+
 </div>
-</html>
+</div>
+
+
+
+
 <script>
     //In order for the text box to render as soon as user
     //enters page, we call switchInput function

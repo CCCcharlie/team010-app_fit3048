7f470b43cdf4149041f86426a561a92fa8f76c47
lche0185 <lche0185@student.monash.edu>
2023-08-20 20:54:46 +1000

    merge the list view with the table

--- templates/Customers/index.php
+++ templates/Customers/index.php
@@ -240,6 +240,86 @@ $checkConnection = function (string $name) {
 
                             <?= $this->Html->link(__('New Customer'), ['action' => 'add'], ['class' => 'btn btn-primary float-right']) ?>
 
+<!--                            view changing button-->
+                            <div class="view-options">
+                                <button id="list-view-btn" class="btn btn-primary">List View</button>
+                                <button id="table-view-btn" class="btn btn-primary">Table View</button>
+                            </div>
+                            <!--                        list view -->
+                            <div  id="customers-list" style="display: none;" >
+                                <div class="row">
+                                    <?php foreach ($customers as $customer): ?>
+                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                            <div class="card">
+                                                <div class="card-header d-flex">
+                                                    <h4 class="card-header-title"><?= h($customer->f_name) ?> <?= h($customer->l_name) ?></h4>
+                                                    <div class="toolbar card-toolbar-tabs ml-auto">
+                                                        <ul class="nav nav-pills" role="tablist">
+                                                            <li class="nav-item">
+                                                                <a class="nav-link active" id="pills-home-tab-<?= $customer->id ?>" data-toggle="pill" href="#pills-home-<?= $customer->id ?>" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+                                                            </li>
+                                                            <li class="nav-item">
+                                                                <a class="nav-link" id="pills-profile-tab-<?= $customer->id ?>" data-toggle="pill" href="#pills-profile-<?= $customer->id ?>" role="tab" aria-controls="pills-profile" aria-selected="false">Technical Details</a>
+                                                            </li>
+                                                            <li class="nav-item">
+                                                                <a class="nav-link" id="pills-contact-tab-<?= $customer->id ?>" data-toggle="pill" href="#pills-contact-<?= $customer->id ?>" role="tab" aria-controls="pills-contact" aria-selected="false">Contact Methods</a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="card-body">
+                                                    <div class="tab-content mb-3" id="pills-tabContent-<?= $customer->id ?>">
+                                                        <div class="tab-pane fade show active" id="pills-home-<?= $customer->id ?>" role="tabpanel" aria-labelledby="pills-home-tab-<?= $customer->id ?>">
+                                                            <br> Status: <?= h($customer->status) ?>
+                                                            <br> Notes: <?= h($customer->notes) ?>
+
+                                                        </div>
+                                                        <div class="tab-pane fade" id="pills-profile-<?= $customer->id ?>" role="tabpanel" aria-labelledby="pills-profile-tab-<?= $customer->id ?>">
+                                                            <!-- Add stuff like technical details and devices. -->
+                                                            <?php
+                                                            if (!empty($customer->devices)) {
+                                                                foreach ($customer->devices as $device) {
+                                                                    echo '<p>Device Model: ' . h($device->device_model) . '</p>';
+                                                                    echo '<p>Technical Details: ' . h($device->technical_details) . '</p>';
+                                                                    echo '<p>Session ID: ' . h($device->session_id) . '</p>';
+                                                                    echo '<p>Transaction ID: ' . h($device->transaction_id) . '</p>';
+
+                                                                }
+                                                            } else {
+                                                                echo '<p>No devices associated with this customer.</p>';
+                                                            }
+                                                            ?>
+                                                        </div>
+
+                                                        <div class="tab-pane fade" id="pills-contact-<?= $customer->id ?>" role="tabpanel" aria-labelledby="pills-contact-tab-<?= $customer->id ?>">
+                                                            <br>Primary E-mail: <?= h($customer->email) ?>
+                                                            <?php
+                                                            if (!empty($customer->commdetails)) {
+                                                                foreach ($customer->commdetails as $commdetail) {
+                                                                    echo '<p>Alternate Contact: ' . h($commdetail->link) . '</p>';
+                                                                }
+                                                            } else {
+                                                                echo '<p>No other contact methods were found</p>';
+                                                            }
+                                                            ?>
+                                                        </div>
+                                                    </div>
+                                                    <?= $this->Html->link(__('View Full Profile'), ['action' => 'view', $customer->id], ['class' => 'btn btn-primary']) ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
+
+                                    <?php if ($this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) === 'Page 1 of 0, showing 0 record(s) out of 0 total'): ?>
+                                        <p>No results found.</p>
+                                    <?php endif; ?>
+
+
+                                </div>
+
+
+                            </div>
+
                             <br>
                             <br>
 
@@ -263,14 +343,18 @@ $checkConnection = function (string $name) {
                         <div id="filtered-content">
                             <!-- content show for assign to me -->
                         </div>
-                        <div class="row">
+
+
+
+<!--                        table view-->
+                        <div class="row"  >
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12  d-flex justify-content-right">
-                                <div class="card mx-auto">
+                                <div class="card mx-auto" id="customers-table">
                                     <div class="card-header">
                                         <h5 class="mb-0">Customer View - All Customers</h5>
                                     </div>
-                                    <div class="card-body">
-                                        <div class="table-responsive">
+                                    <div class="card-body"  >
+                                        <div class="table-responsive" >
                                             <table id="customers" class="table table-striped table-bordered second" style="width:100%">
                                                 <thead>
                                                 <tr>
@@ -332,7 +416,7 @@ $checkConnection = function (string $name) {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+
                         </div>
 
 
@@ -490,7 +574,25 @@ $checkConnection = function (string $name) {
                         }
                     </style>
 
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    const listViewBtn = document.getElementById('list-view-btn');
+                    const tableViewBtn = document.getElementById('table-view-btn');
+                    const customersList = document.getElementById('customers-list');
+                    const customersTable = document.getElementById('customers-table');
 
+                    listViewBtn.addEventListener('click', function() {
+                        customersList.style.display = 'flex';
+                        customersTable.style.display = 'none';
+                    });
+
+                    tableViewBtn.addEventListener('click', function() {
+                        customersList.style.display = 'none';
+                        customersTable.style.display = 'table';
+                    });
+                });
+
+            </script>
 
 </body>
 

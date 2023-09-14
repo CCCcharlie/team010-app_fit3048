f9cfe0b327e09bd9dc20dec1d2b1d366ffcd2e88
adun0008 <adun0008@student.monash.edu>
2023-09-14 10:24:53 +1000

    Cleaned up add and edit on the Counsellors page.
    Noted error: Redirect not working after editing a Counsellor.

--- templates/Counsellors/add.php
+++ templates/Counsellors/add.php
@@ -7,10 +7,6 @@
 ?>
 <div class="row">
     <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Counsellors'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
     </aside>
     <div class="container-fluid dashboard-content">
         <div class="counsellors form content">
@@ -52,38 +48,39 @@
                             <fieldset>
                                 <div class="form-group">
                                     <?= $this->Form->label('f_name', 'First Name', ['class' => 'col-form-label']) ?>
-                                    <?= $this->Form->input('f_name', ['class' => 'form-control']) ?>
+                                    <?= $this->Form->input('f_name', ['class' => 'form-control', 'maxlength' => 32]) ?>
                                     <!-- Display validation error for the 'f_name' field -->
                                     <?= $this->Form->error('f_name'); ?>
                                 </div>
                                 <div class="form-group">
                                     <?= $this->Form->label('l_name', 'Last Name', ['class' => 'col-form-label']) ?>
-                                    <?= $this->Form->input('l_name', ['class' => 'form-control']) ?>
+                                    <?= $this->Form->input('l_name', ['class' => 'form-control', 'maxlength' => 32]) ?>
                                     <!-- Display validation error for the 'l_name' field -->
                                     <?= $this->Form->error('l_name'); ?>
                                 </div>
                                 <div class="form-group">
                                     <?= $this->Form->label('notes', 'Notes', ['class' => 'col-form-label']) ?>
-                                    <?= $this->Form->input('notes', ['class' => 'form-control']) ?>
+                                    <?= $this->Form->input('notes', ['class' => 'form-control', 'maxlength' => 150]) ?>
                                     <!-- Display validation error for the 'notes' field -->
                                     <?= $this->Form->error('notes'); ?>
                                 </div>
-                                <!-- Uncomment and style the following controls as needed -->
-                                <!--
-    <div class="form-group">
-        <?= $this->Form->label('cust_id', 'Customer', ['class' => 'col-form-label']) ?>
-        <?= $this->Form->input('cust_id', ['class' => 'form-control', 'options' => $customers]) ?>
-    </div>
-    -->
+                                <div class="form-group">
+                                    <?= $this->Form->label('cust_id', 'Customer', ['class' => 'col-form-label']) ?>
+                                    <?= $this->Form->input('cust_id', ['options' => $customers, 'empty' => true, 'class' => 'form-control']) ?>
+                                    <!-- Display validation error for the 'cust_id' field -->
+                                    <?= $this->Form->error('cust_id'); ?>
+                                </div>
                                 <div class="form-group">
                                     <?= $this->Form->label('contact', 'Contact', ['class' => 'col-form-label']) ?>
-                                    <?= $this->Form->input('contact', ['class' => 'form-control']) ?>
+                                    <?= $this->Form->input('contact', ['class' => 'form-control', 'maxlength' => 500]) ?>
                                     <!-- Display validation error for the 'contact' field -->
                                     <?= $this->Form->error('contact'); ?>
                                 </div>
                             </fieldset>
                             <div class="form-group d-flex justify-content-between align-items-center">
-                                <?= $this->Html->link(__('Return to Customers'), ['controller' => 'Customers', 'action' => 'index'], ['class' => 'btn btn-rounded btn-secondary']) ?>
+                                <div class="form-group d-flex justify-content-between align-items-center">
+                                    <?= $this->Html->link(__('Go Back'), ['controller' => 'Customers', 'action' => 'view', $custId], ['class' => 'btn btn-rounded btn-secondary']) ?>
+                                </div>
                                 <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
                             </div>
                         </div>

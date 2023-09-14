5b7a2398bd251b0d70ed39037b4a9ccb5fc3fcb9
lche0185 <lche0185@student.monash.edu>
2023-09-14 11:29:00 +1000

    implement the deescalate/ blocked

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -315,7 +315,12 @@ class TicketsController extends AppController
         // update "escalate" to true（1）
         $ticket->escalate = true;
         $ticket->staff_id = 2;
+//        debug($id);
+//      debug($identity->get('id'));
+//        exit();
 
+
+        // Pass $id to the view as selectedTicketId
 
         // save
         if ($this->Tickets->save($ticket)) {
@@ -325,9 +330,9 @@ class TicketsController extends AppController
 
 
         } else {
-            $this->Flash->error(__('Escalation failed. Errors: {0}', print_r($ticket->getErrors(), true)));
+//            $this->Flash->error(__('Escalation failed. Errors: {0}', print_r($ticket->getErrors(), true)));
 
-//            $this->Flash->error(__('Escalation failed.'));
+            $this->Flash->error(__('Escalation failed.'));
         }
 
         //
@@ -336,8 +341,11 @@ class TicketsController extends AppController
 
     public function undoEscalate($id)
     {
+
+
         $identity = $this->request->getAttribute('authentication')->getIdentity();
         // base on id get the ticket
+
         $ticket = $this->Tickets->get($id);
 
         // update "escalate" to false (0)

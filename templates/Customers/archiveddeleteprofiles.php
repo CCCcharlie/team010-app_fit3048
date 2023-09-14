f55bd78270f9c3a34fa60a4e9ac7843400eb2674
adun0008 <adun0008@student.monash.edu>
2023-09-14 13:00:14 +1000

    Fix.

--- templates/Customers/archiveddeleteprofiles.php
+++ templates/Customers/archiveddeleteprofiles.php
@@ -135,14 +135,6 @@ to get the name or any value in the staff table, use the get and then the name o
                 <br>
                 <div class="row">
                     <div class="col-md-6 ">
-                        <h3 style="color: midnightblue"> Filter: </h3>
-                        <?= $this->Form->create(null, ['url' => ['controller' => 'Customers', 'action' => 'index'], 'type' => 'get', 'class' => 'form-inline']) ?>
-                        <div class="form-group mr-2">
-                            <?= $this->Form->input('search', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Search...']) ?>
-                        </div>
-                        <div class="btn-group ml-2">
-                            <?= $this->Form->button(__('Search'), ['class' => 'btn btn-primary custom-button']) ?>
-                            <?= $this->Form->end() ?>
 
 
                         </div>

f2aaa7a93038db127074cf3ac25f02b5e2dacbb2
lche0185 <lche0185@student.monash.edu>
2023-10-01 16:59:44 +1100

    fix forget password not showing

--- templates/Auth/login.php
+++ templates/Auth/login.php
@@ -68,8 +68,7 @@ $this->assign('title', 'Login');
                         echo $this->Form->hidden('email', ['value' => '']); // Hide email input
                         echo $this->Form->hidden('password', ['value' => '']); // Hide password input
 
-                        // Add the "Forgot Password?" link
-                        echo $this->Html->link('Forgot password?', ['controller' => 'Auth', 'action' => 'forgetPassword'], ['class' => 'button button-outline']);
+
                     } else {
                         // If not locked out or lockout period has expired, display the form fields and the login button
                         echo $this->Form->control('email', [
@@ -96,6 +95,8 @@ $this->assign('title', 'Login');
                                 'data-callback' => 'onSubmit',
                                 'data-action' => 'submit'
                             ]);
+                            // Add the "Forgot Password?" link
+                            echo $this->Html->link('Forgot password?', ['controller' => 'Auth', 'action' => 'forgetPassword'], ['class' => 'button button-outline','style'=>"display: flex; justify-content: center; align-items: center;"]);
                         }
                     }
                     echo $this->Form->end();

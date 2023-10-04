6b9f7a34506ad5ebc165fa8bf13a8a8fd91d8bf0
lche0185 <lche0185@student.monash.edu>
2023-10-04 15:43:29 +1100

    added unescalated feature in escalatetome page / showing all the escalated customer in the page (remember to remove team010-app_fit3048 in live )/change the flash message/add unescalate button on escalate to me page

--- templates/Customers/escalatetome.php
+++ templates/Customers/escalatetome.php
@@ -293,7 +293,7 @@ to get the name or any value in the staff table, use the get and then the name o
                         <script>
                             document.getElementById("undobutton-<?= $customer->id ?>").addEventListener('click', function () {
                                 // Get the customer ID
-
+                                var customerId = " "
                                 var customerId = <?= $customer->id ?>;
                                 console.log(customerId)
 

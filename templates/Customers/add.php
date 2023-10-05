79641a69bdaf46de12a7a52e7d9418269643d25f
mwei0010 <mwei0010@student.monash.edu>
2023-10-05 14:35:38 +1100

    archived customer table action buttons fixed

--- templates/Customers/add.php
+++ templates/Customers/add.php
@@ -38,7 +38,7 @@
                     <div class="section-block" id="basicform">
                     </div>
                     <div class="card">
-                        <h5 class="card-header" style="color: midnightblue">Add a New Customer</h5>
+                        <h4 class="card-header" style="color: midnightblue">Add a New Customer</h4>
                         <div class="card-body">
                             <?= $this->Form->create($customer, ['onsubmit' => 'return validateEmail();']) ?>
                             <div class="form-group">
